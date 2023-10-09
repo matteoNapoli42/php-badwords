296b0bf7ad2433375fa3d1ee3b48bcba4f879aba
matteoNapoli42 <matteon4242@gmail.com>
2023-10-09 16:42:22 +0200

    create a layout for user inputs

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP BADWORDS</title>
 </head>
+
 <body>
-    
-
-
+    <h1>Censura una parola</h1>
+    <form action="script.php">
+        <input type="text" placeholder="Scrivi una frase">
+        <input type="text" placeholder="Scrivi quale parola vuoi bannare">
+        <button type="submit">Premi per censurare</button>
+    </form>
 </body>
+
 </html>
